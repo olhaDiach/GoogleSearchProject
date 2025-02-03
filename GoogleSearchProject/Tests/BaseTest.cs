2e989f636a3c1a71a3211ae2992510c7799b4f32
olhaDiach <o94840070@gmail.com>
2025-02-03 20:28:35 +0200

    add cromeOptions

--- GoogleSearchProject/Tests/BaseTest.cs
+++ GoogleSearchProject/Tests/BaseTest.cs
@@ -12,8 +12,15 @@ namespace GoogleSearchProject.Tests
         [SetUp]
         public void Setup()
         {
-            var options = new ChromeOptions();
-            Driver = new ChromeDriver(options);
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--headless");  
+            chromeOptions.AddArguments("--disable-gpu");  
+            chromeOptions.AddArguments("--no-sandbox");  
+            chromeOptions.AddArguments("--disable-dev-shm-usage"); 
+            chromeOptions.AddArguments("--remote-debugging-port=9222");  
+            chromeOptions.AddArguments("--window-size=1920,1080");  
+            chromeOptions.AddArguments("--disable-features=VizDisplayCompositor");  
+            Driver = new ChromeDriver(chromeOptions);
             Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
         }
 
