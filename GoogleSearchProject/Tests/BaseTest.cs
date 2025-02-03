89a178e8e9f7e3d766d21ab2d08784b033131ed4
olhaDiach <o94840070@gmail.com>
2025-02-03 20:35:08 +0200

    Fix for "user data directory already in use" issue

--- GoogleSearchProject/Tests/BaseTest.cs
+++ GoogleSearchProject/Tests/BaseTest.cs
@@ -19,7 +19,9 @@ namespace GoogleSearchProject.Tests
             chromeOptions.AddArguments("--disable-dev-shm-usage"); 
             chromeOptions.AddArguments("--remote-debugging-port=9222");  
             chromeOptions.AddArguments("--window-size=1920,1080");  
-            chromeOptions.AddArguments("--disable-features=VizDisplayCompositor");  
+            chromeOptions.AddArguments("--disable-features=VizDisplayCompositor");
+            // Fix for "user data directory already in use" issue
+            chromeOptions.AddArguments($"--user-data-dir=/tmp/chrome-profile-{Guid.NewGuid()}");
             Driver = new ChromeDriver(chromeOptions);
             Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
         }
